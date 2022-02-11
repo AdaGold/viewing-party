bb5399852443caefee44935d4847dc2da3ab8b24
Becca Elenzil <becca@adadevelopersacademy.org>
2022-02-11 12:14:27 -0800

    Scaffold test for wave 3 and 5

--- tests/test_wave_03.py
+++ tests/test_wave_03.py
@@ -54,9 +54,10 @@ def test_friends_unique_movies_not_duplicated():
 
     # Arrange
     assert len(friends_unique_movies) == 3
-    assert INTRIGUE_3 in friends_unique_movies
-    assert HORROR_1 in friends_unique_movies
-    assert FANTASY_4 in friends_unique_movies
+
+    # *************************************************************************************************
+    # ****** Add assertions here to test that the correct movies are in friends_unique_movies **********
+    # **************************************************************************************************
 
 @pytest.mark.skip()
 def test_friends_not_unique_movies():
--- tests/test_wave_05.py
+++ tests/test_wave_05.py
@@ -53,11 +53,9 @@ def test_new_genre_rec_from_empty_friends():
         ]
     }
 
-    # Act
-    recommendations = get_new_rec_by_genre(sonyas_data)
-
-    # Assert
-    assert len(recommendations) == 0
+    # *********************************************************************
+    # ****** Complete the Act and Assert Portions of theis tests **********
+    # *********************************************************************
 
 @pytest.mark.skip()
 def test_unique_rec_from_favorites():
