56321f0c8741d2ea918f10019fda9c11e962373b
Becca Elenzil <becca@adadevelopersacademy.org>
2022-02-11 11:28:44 -0800

    Use clean_wave_2_data instead of hardcoded

--- tests/test_wave_02.py
+++ tests/test_wave_02.py
@@ -1,53 +1,19 @@
 import pytest
 from viewing_party.party import *
-import copy
+from tests.test_constants import *
+
 
 @pytest.mark.skip()
 def test_calculates_watched_average_rating():
     # Arrange
-    janes_data = {
-        'watched': [
-            {
-                'title': 'The Lord of the Functions: The Fellowship of the Function', 
-                'genre': 'Fantasy', 
-                'rating': 4.8
-            }, 
-            {
-                'title': 'The Lord of the Functions: The Two Parameters', 
-                'genre': 'Fantasy', 
-                'rating': 4.0
-            }, 
-            {
-                'title': 'The Lord of the Functions: The Return of the Value',
-                'genre': 'Fantasy', 
-                'rating': 4.0
-            }, 
-            {
-                'title': 'The JavaScript and the React', 
-                'genre': 'Action', 
-                'rating': 2.2
-            }, 
-            {
-                'title': 'Recursion', 
-                'genre': 'Intrigue', 
-                'rating': 2.0
-            }, 
-            {
-                'title': 'Instructor Student TA Manager', 
-                'genre': 'Intrigue', 
-                'rating': 4.5
-            }
-        ]
-    }
-
-    janes_data_before = copy.deepcopy(janes_data)
+    janes_data = clean_wave_2_data()
 
     # Act
     average = get_watched_avg_rating(janes_data)
 
     # Assert
     assert average == pytest.approx(3.58333)
-    assert janes_data == janes_data_before
+    assert janes_data == clean_wave_2_data()
 
 @pytest.mark.skip()
 def test_empty_watched_average_rating_is_zero():
@@ -65,48 +31,14 @@ def test_empty_watched_average_rating_is_zero():
 @pytest.mark.skip()
 def test_most_watched_genre():
     # Arrange
-    janes_data = {
-        'watched': [
-            {
-                'title': 'The Lord of the Functions: The Fellowship of the Function', 
-                'genre': 'Fantasy', 
-                'rating': 4.8
-            }, 
-            {
-                'title': 'The Lord of the Functions: The Two Parameters', 
-                'genre': 'Fantasy', 
-                'rating': 4.0
-            }, 
-            {
-                'title': 'The Lord of the Functions: The Return of the Value',
-                'genre': 'Fantasy', 
-                'rating': 4.0
-            }, 
-            {
-                'title': 'The JavaScript and the React', 
-                'genre': 'Action', 
-                'rating': 2.2
-            }, 
-            {
-                'title': 'Recursion', 
-                'genre': 'Intrigue', 
-                'rating': 2.0
-            }, 
-            {
-                'title': 'Instructor Student TA Manager', 
-                'genre': 'Intrigue', 
-                'rating': 4.5
-            }
-        ]
-    }
-    janes_data_before = copy.deepcopy(janes_data)
+    janes_data = clean_wave_2_data()
 
     # Act
     popular_genre = get_most_watched_genre(janes_data)
 
     # Assert
     assert popular_genre == "Fantasy"
-    assert janes_data == janes_data_before
+    assert janes_data == clean_wave_2_data()
 
 @pytest.mark.skip()
 def test_genre_is_None_if_empty_watched():
