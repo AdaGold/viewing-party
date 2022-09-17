393702586780a12aad3dcedb389a38f6e9eb15d7
Emily <sonemily+github@gmail.com>
2022-09-17 16:54:56 -0400

    Create watch_movie function to move watchlist movies to watched movies. Update test to ensure correct movie was added.

--- tests/test_wave_01.py
+++ tests/test_wave_01.py
@@ -100,7 +100,7 @@ def test_adds_movie_to_user_watchlist():
     assert updated_data["watchlist"][0]["genre"] == GENRE_1
     assert updated_data["watchlist"][0]["rating"] == RATING_1
 
-@pytest.mark.skip()
+# @pytest.mark.skip()
 def test_moves_movie_from_watchlist_to_empty_watched():
     # Arrange
     janes_data = {
@@ -119,12 +119,16 @@ def test_moves_movie_from_watchlist_to_empty_watched():
     assert len(updated_data["watchlist"]) == 0
     assert len(updated_data["watched"]) == 1
     
-    raise Exception("Test needs to be completed.")
+    # raise Exception("Test needs to be completed.")
     # *******************************************************************************************
     # ****** Add assertions here to test that the correct movie was added to "watched" **********
     # *******************************************************************************************
+    assert updated_data["watched"][0]["title"] == MOVIE_TITLE_1
+    assert updated_data["watched"][0]["genre"] == GENRE_1
+    assert updated_data["watched"][0]["rating"] == RATING_1
 
-@pytest.mark.skip()
+
+# @pytest.mark.skip()
 def test_moves_movie_from_watchlist_to_watched():
     # Arrange
     movie_to_watch = HORROR_1
@@ -143,10 +147,11 @@ def test_moves_movie_from_watchlist_to_watched():
     assert len(updated_data["watchlist"]) == 1
     assert len(updated_data["watched"]) == 2
     
-    raise Exception("Test needs to be completed.")
+    # raise Exception("Test needs to be completed.")
     # *******************************************************************************************
     # ****** Add assertions here to test that the correct movie was added to "watched" **********
     # *******************************************************************************************
+    assert updated_data["watched"][-1]["title"] == movie_to_watch["title"]
 
 @pytest.mark.skip()
 def test_does_nothing_if_movie_not_in_watchlist():
--- viewing_party/party.py
+++ viewing_party/party.py
@@ -9,13 +9,25 @@ def create_movie(title, genre, rating):
         "rating": rating
     }
 
+
 def add_to_watched(user_data, movie):
     user_data["watched"].append(movie)
     return user_data
 
+
 def add_to_watchlist(user_data, movie):
     user_data["watchlist"].append(movie)
     return user_data
+
+
+def watch_movie(user_data, movie_title):
+    for movie in user_data["watchlist"]:
+        if movie["title"] == movie_title:
+            user_data["watchlist"].remove(movie)
+            user_data["watched"].append(movie)
+    return user_data
+
+
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
