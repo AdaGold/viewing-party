a2d6da81010230d54e57f79595bc58fa06420daa
Emily <sonemily+github@gmail.com>
2022-09-17 17:08:13 -0400

    Pass all test_wave_01 tests. watch_movie does nothing if the movie is not in the watchlist.

--- tests/test_wave_01.py
+++ tests/test_wave_01.py
@@ -153,7 +153,7 @@ def test_moves_movie_from_watchlist_to_watched():
     # *******************************************************************************************
     assert updated_data["watched"][-1]["title"] == movie_to_watch["title"]
 
-@pytest.mark.skip()
+# @pytest.mark.skip()
 def test_does_nothing_if_movie_not_in_watchlist():
     # Arrange
     movie_to_watch = HORROR_1
