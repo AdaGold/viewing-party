c064f4b38e817a2fee3dd32e13661cbc44ef712e
Emily <sonemily+github@gmail.com>
2022-09-17 20:50:17 -0400

    Add exception handling for empty watch list for get_most_watched_genre function.

--- tests/test_wave_02.py
+++ tests/test_wave_02.py
@@ -39,7 +39,7 @@ def test_most_watched_genre():
     assert popular_genre == "Fantasy"
     assert janes_data == clean_wave_2_data()
 
-@pytest.mark.skip()
+# @pytest.mark.skip()
 def test_genre_is_None_if_empty_watched():
     # Arrange
     janes_data = {
--- viewing_party/party.py
+++ viewing_party/party.py
@@ -44,13 +44,19 @@ def get_watched_avg_rating(user_data):
 
 def get_most_watched_genre(user_data):
     watched_genres = {}
+    
+    if len(user_data) == 0:
+        return None
+    
     for movie in user_data["watched"]:
         if movie["genre"] not in watched_genres:
             watched_genres[movie["genre"]] = 0
         else:
             watched_genres[movie["genre"]] += 1
+    
     most_watched = 0
     most_watched_genre = None
+    
     for genre, frequency in watched_genres.items():
         if frequency > most_watched:
             most_watched = frequency
