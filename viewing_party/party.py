bd6ec7acee97a41870ba81afceaeaaecbde6264c
Emily <sonemily+github@gmail.com>
2022-09-22 16:19:20 -0400

    Refactor get_watched_avg_rating(user_data) to run if len(user_data[watched]) > 0.

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -34,12 +34,12 @@ def watch_movie(user_data, movie_title):
 
 def get_watched_avg_rating(user_data):
     avg_rating = 0
-    if len(user_data["watched"]) == 0:
-        return avg_rating
 
-    for movie in user_data["watched"]:
-        avg_rating += movie["rating"]
-    return avg_rating / len(user_data["watched"])
+    if len(user_data["watched"]) > 0:
+        for movie in user_data["watched"]:
+            avg_rating += movie["rating"]
+        return avg_rating / len(user_data["watched"])
+    return avg_rating
 
 
 def get_most_watched_genre(user_data):
