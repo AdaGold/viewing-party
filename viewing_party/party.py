ae446a22f73f74438ec9b147618962bc17cc010b
Catherine Bandarchuk <kate@sealucky.com>
2022-10-22 21:10:47 -0700

    Comlete wave_test2.py. Create function for wave2.

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,4 +1,5 @@
 # ------------- WAVE 1 --------------------
+import statistics
 
 def create_movie(title, genre, rating):
     new_movie = dict()
@@ -25,10 +26,6 @@ def add_to_watchlist(user_data, movie):
     return updated_user_data
 
 def watch_movie(user_data, movie_title):
-    #user_data = {
-    #    "watchlist": [],
-    #    "watched": []
-    #}
     if movie_title == None or user_data["watchlist"] == None:
         return user_data
     else: 
@@ -40,8 +37,27 @@ def watch_movie(user_data, movie_title):
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
+def get_watched_avg_rating(user_data):
+    avg_rating = 0.0
+    sum_ratings = 0
+    if len(user_data["watched"]) == 0:
+        return 0.0
+    else:
+        for element in user_data["watched"]:
+            sum_ratings += element["rating"]
+        avg_rating = sum_ratings/len(user_data["watched"])
+        return avg_rating
 
-
+def get_most_watched_genre(user_data):
+    most_watched_genre = ""
+    list_of_genre = list()
+    if len(user_data["watched"]) == 0:
+        return None
+    else:
+        for i in range(len(user_data["watched"])):
+            list_of_genre.append(user_data["watched"][i]["genre"])
+        most_watched_genre = statistics.mode(list_of_genre)
+        return most_watched_genre
 # -----------------------------------------
 # ------------- WAVE 3 --------------------
 # -----------------------------------------
