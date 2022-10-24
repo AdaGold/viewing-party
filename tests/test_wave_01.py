5f9b3ee2bb666688479bc759cdfd4b8387da0cc3
Catherine Bandarchuk <kate@sealucky.com>
2022-10-23 20:20:30 -0700

    create functions for wave_03 test. Fixes or complite test_wave_03.py

--- tests/test_wave_01.py
+++ tests/test_wave_01.py
@@ -4,7 +4,6 @@ import pytest
 from viewing_party.party import *
 from tests.test_constants import *
 
-#@pytest.mark.skip()
 def test_create_successful_movie():
     # Arrange
     movie_title = MOVIE_TITLE_1
@@ -19,7 +18,6 @@ def test_create_successful_movie():
     assert new_movie["genre"] == GENRE_1
     assert new_movie["rating"] == pytest.approx(RATING_1)
 
-#@pytest.mark.skip()
 def test_create_no_title_movie():
     # Arrange
     movie_title = None
@@ -32,7 +30,6 @@ def test_create_no_title_movie():
     # Assert
     assert new_movie is None
 
-#@pytest.mark.skip()
 def test_create_no_genre_movie():
     # Arrange
     movie_title = "Title A"
@@ -45,7 +42,6 @@ def test_create_no_genre_movie():
     # Assert
     assert new_movie is None
 
-#@pytest.mark.skip()
 def test_create_no_rating_movie():
     # Arrange
     movie_title = "Title A"
@@ -58,7 +54,6 @@ def test_create_no_rating_movie():
     # Assert
     assert new_movie is None
 
-#@pytest.mark.skip()
 def test_adds_movie_to_user_watched():
     # Arrange
     movie = {
@@ -79,7 +74,6 @@ def test_adds_movie_to_user_watched():
     assert updated_data["watched"][0]["genre"] == GENRE_1
     assert updated_data["watched"][0]["rating"] == RATING_1
 
-#@pytest.mark.skip()
 def test_adds_movie_to_user_watchlist():
     # Arrange
     movie = {
@@ -100,7 +94,6 @@ def test_adds_movie_to_user_watchlist():
     assert updated_data["watchlist"][0]["genre"] == GENRE_1
     assert updated_data["watchlist"][0]["rating"] == RATING_1
 
-#@pytest.mark.skip()
 def test_moves_movie_from_watchlist_to_empty_watched():
     # Arrange
     janes_data = {
@@ -122,8 +115,6 @@ def test_moves_movie_from_watchlist_to_empty_watched():
     assert updated_data["watched"][0]["genre"] == GENRE_1
     assert updated_data["watched"][0]["rating"] == RATING_1
     
-    
-#@pytest.mark.skip()
 def test_moves_movie_from_watchlist_to_watched():
     # Arrange
     movie_to_watch = HORROR_1
@@ -144,12 +135,7 @@ def test_moves_movie_from_watchlist_to_watched():
     assert updated_data["watched"][0]["title"] == movie_to_watch["title"]
     assert updated_data["watched"][0]["genre"] == movie_to_watch["genre"]
     assert updated_data["watched"][0]["rating"] == movie_to_watch["rating"]
-    #raise Exception("Test needs to be completed.")
-    # *******************************************************************************************
-    # ****** Add assertions here to test that the correct movie was added to "watched" **********
-    # *******************************************************************************************
-
-#@pytest.mark.skip()
+    
 def test_does_nothing_if_movie_not_in_watchlist():
     # Arrange
     movie_to_watch = HORROR_1
--- viewing_party/party.py
+++ viewing_party/party.py
@@ -61,7 +61,41 @@ def get_most_watched_genre(user_data):
 # -----------------------------------------
 # ------------- WAVE 3 --------------------
 # -----------------------------------------
-
+def get_unique_watched(user_data):
+    friends_movie_list = list()
+    unique_movies = list()
+    if len(user_data["watched"]) == 0:
+        unique_movies = []
+        return unique_movies
+    else:
+        for i in range(len(user_data["friends"])):
+            for j in range(len(user_data["friends"][i]["watched"])):
+                friends_movie_list.append(user_data["friends"][i]["watched"][j]["title"])
+        friends_movie_set = set(friends_movie_list)
+        for k in range(len(user_data["watched"])):
+            if user_data["watched"][k]["title"] not in friends_movie_set:
+                unique_movies.append(user_data["watched"][k])
+        return unique_movies
+    
+def get_friends_unique_watched(user_data):
+    friends_movie_list = list()
+    friends_unique_movies = list()
+    if len(user_data["watched"]) == 0:
+        for i in range(len(user_data["friends"])):
+            for j in range(len(user_data["friends"][i]["watched"])):
+                if (friends_movie_list[i] not in friends_unique_movies):
+                    friends_movie_list.append(user_data["friends"][i]["watched"][j])
+        #friends_unique_movies = set(friends_movie_list)
+        return friends_unique_movies
+    else:
+        for i in range(len(user_data["friends"])):
+            for j in range(len(user_data["friends"][i]["watched"])):
+                friends_movie_list.append(user_data["friends"][i]["watched"][j])
+        for k in range(len(friends_movie_list)):
+            if (friends_movie_list[k] not in user_data["watched"]) and ((friends_movie_list[k] not in friends_unique_movies)):
+                friends_unique_movies.append(friends_movie_list[k])
+        #friends_unique_movies_set = set(friends_unique_movies)
+        return friends_unique_movies#_set
         
 # -----------------------------------------
 # ------------- WAVE 4 --------------------
