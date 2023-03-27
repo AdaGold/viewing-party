a815d3677fc0ad6d77e50067c2d43fbaacc5b2d9
Charlie <charliethemushi@gmail.com>
2023-03-27 12:16:53 -0700

    created notes to build first function

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,6 +1,13 @@
 # ------------- WAVE 1 --------------------
 
 def create_movie(title, genre, rating):
+    """
+    first function create movie
+    function takes 3 paramaters/attributes, titles, genre, rating
+    if three attributers are truthy, return dictionary
+    if title falsy, genre is falsy or rating is falsy, these should return none
+  
+    """
     pass
 
 # -----------------------------------------
