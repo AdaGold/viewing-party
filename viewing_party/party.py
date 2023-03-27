28bdf3f6e62814b23a519ce736d5196c38dcb731
Hannah <hannahrwatkins@gmail.com>
2023-03-27 13:39:01 -0400

    changes are happening

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -15,6 +15,11 @@ def create_movie(title, genre, rating):
     return new_movie
 
 
+def add_to_watched(user_data, movie):
+    user_data["watched"].append(movie) 
+    return user_data
+
+
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
