dacd5a46094eb0b6c7ccaf6ba6a3db283dba9933
Charlie <charliethemushi@gmail.com>
2023-03-27 12:44:11 -0700

    started first function for title, genre, rating

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -6,8 +6,17 @@ def create_movie(title, genre, rating):
     function takes 3 paramaters/attributes, titles, genre, rating
     if three attributers are truthy, return dictionary
     if title falsy, genre is falsy or rating is falsy, these should return none
-  
+    
     """
+    #new_movie = {}
+
+    if not title | genre:
+        return true
+    if rating != int:
+        return none
+
+    return title, genre, rating
+    
     pass
 
 # -----------------------------------------
