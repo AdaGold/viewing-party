3f1652ce7afc0c8c6023b849b81a9d7466e6bacd
Selene Castillo <secast@live.com>
2023-03-27 17:08:43 -0700

    4th funk done, added assertions

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -7,7 +7,7 @@
     if title falsy, genre is falsy or rating is falsy, these should return none
     
 """
-
+# 1st function
 def create_movie(title, genre, rating):
     
     #possible us of isinstance method
@@ -28,11 +28,44 @@ def create_movie(title, genre, rating):
 
     return new_movie
 
-    
-    
+# 2nd function
 def add_to_watched(user_data, movie):
     (user_data["watched"]).append(movie)
     return user_data
+
+# watch_movie testing info, please leave commented incase it breaks
+# user_data = {
+#     "watchlist": [
+#         {"title": "Land Before Time"},
+#         {"title": "Spirited Away"}
+#     ],
+#     "watched": [
+#             {"title": "Lord of the Rings"},
+#             {"title": "Parasyte"},
+#             {"title": "Harry Potter"},
+#             {"title": "Ready Player One"}
+#     ]
+# }
+# title = "Land Before Time"
+
+# 4th function
+def watch_movie(user_data, title):
+    move_movie = False
+    index = 0
+    movie = None
+    for index in range(len(user_data["watchlist"])):
+        if title == user_data["watchlist"][index]["title"]:
+            move_movie = True
+            movie_index = index
+            movie = user_data["watchlist"][index]
+            print(user_data["watchlist"][index])
+    if move_movie:
+        (user_data["watched"]).append(movie)
+        del user_data["watchlist"][movie_index]
+    return user_data
+# watch_movie testing info, please leave commented incase it breaks
+# watch_movie(user_data, title)
+
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
