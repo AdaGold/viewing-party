ea58480884d35ed997289d738489ceeedf30dc56
Arianna Walden <nedlawa@gmail.com>
2023-03-28 12:35:15 -0700

    implemented first 2 funcs; add all wave signatures

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,23 +1,48 @@
 # ------------- WAVE 1 --------------------
 
 def create_movie(title, genre, rating):
-    pass
+    new_movie = {}
+    if not title or not genre or not rating:
+        return None
+    else:
+        new_movie["title"] = title
+        new_movie["genre"] = genre
+        new_movie["rating"] = rating
+    return new_movie
+
+def add_to_watched(user_data, movie):
+    user_data["watched"] += [movie]
+    return user_data
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
+def get_watched_avg_rating(user_data):
+    pass
 
+def get_most_watched_genre(user_data):
+    pass
 
 # -----------------------------------------
 # ------------- WAVE 3 --------------------
 # -----------------------------------------
+def get_unique_watched(user_data):
+    pass
 
+def get_friends_unique_watched(user_data):
+    pass
         
 # -----------------------------------------
 # ------------- WAVE 4 --------------------
 # -----------------------------------------
-
+def get_available_recs(user_data):
+    pass
 # -----------------------------------------
 # ------------- WAVE 5 --------------------
 # -----------------------------------------
 
+def get_new_rec_by_genre(user_data):
+    pass
+
+def get_rec_from_favorites(user_data):
+    pass
