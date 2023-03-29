bae0b6da72dc425bcea255c64ee3fd7ec1cc84df
Alejandra <adom2128@gmail.com>
2023-03-29 15:10:07 -0600

    formatting changes to wave 1 and 2

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,9 +1,10 @@
 # ------------- WAVE 1 --------------------
-# git testing
+
 
 def create_movie(title, genre, rating):
     if not title or not genre or not rating:
         return None
+    
     else:
         movie = {}
         movie["title"] = title
@@ -12,16 +13,19 @@ def create_movie(title, genre, rating):
 
     return movie
 
+
 def add_to_watched(user_data, movie):
     user_data["watched"].append(movie)
 
     return user_data
 
+
 def add_to_watchlist(user_data, movie):
     user_data["watchlist"].append(movie)
 
     return user_data
-        
+
+
 def watch_movie(user_data, title):
     new_watchlist = []
     for i in range(len(user_data["watchlist"])):
@@ -32,8 +36,6 @@ def watch_movie(user_data, title):
     user_data["watchlist"] = new_watchlist
 
     return user_data
-            
-
 
 
 # -----------------------------------------
@@ -42,35 +44,41 @@ def watch_movie(user_data, title):
 def get_watched_avg_rating(user_data):
     ratings_sum = 0
     movie_count = 0
+
     if not user_data["watched"]:
         return 0
-    
+
     for movie in user_data["watched"]:
         ratings_sum += movie["rating"]
         movie_count += 1
+
     return ratings_sum/movie_count
+
 
 def get_most_watched_genre(user_data):
     genre_count = {}
-    if user_data["watched"]:
-        for movie in user_data["watched"]:
-            if movie["genre"] in genre_count:
-                genre_count[movie["genre"]] += 1
-            else:
-                genre_count[movie["genre"]] = 1
-    
-        highest_rated_count = max(genre_count.values())
-        for genre, count in genre_count.items():
-            if count == highest_rated_count:
-                return genre
 
-    return None
+    if not user_data["watched"]:
+        return None
+
+    for movie in user_data["watched"]:
+        if movie["genre"] in genre_count:
+            genre_count[movie["genre"]] += 1
+        else:
+            genre_count[movie["genre"]] = 1
+
+    highest_rated_count = max(genre_count.values())
+    for genre, count in genre_count.items():
+        if count == highest_rated_count:
+    
+            return genre
 
 # -----------------------------------------
 # ------------- WAVE 3 --------------------
 # -----------------------------------------
 
-        
+
+
 # -----------------------------------------
 # ------------- WAVE 4 --------------------
 # -----------------------------------------
@@ -78,4 +86,3 @@ def get_most_watched_genre(user_data):
 # -----------------------------------------
 # ------------- WAVE 5 --------------------
 # -----------------------------------------
-
