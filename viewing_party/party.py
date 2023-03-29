8e3a180acbefab2ffb898e55c8647f5c6e6cbf96
Charlie <charliethemushi@gmail.com>
2023-03-28 21:27:40 -0700

    fixed second function in wave3, adding notes written for wave 4

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -137,12 +137,85 @@ def get_most_watched_genre(user_data):
 # }
 
 
+#wave 3
+#function 2
 
-# -----------------------------------------
-# ------------- WAVE 3 --------------------
-# -----------------------------------------
 
-        
+def get_friends_unique_watched(user_data):
+
+    user_watched = set([movie["title"] for movie in user_data["watched"]])
+    friends_list = []
+
+    for friend in user_data["friends"]:
+        for movie in friend["watched"]:
+            if movie["title"] not in user_watched and movie not in friends_list:
+                friends_list.append(movie)
+
+    return friends_list
+
+"""
+does not remove duplicates
+    user = user_data["watched"]
+    friends = user_data["friends"] 
+
+    friends_list = []
+
+    #i = watched
+    for i_dict in friends:
+        movies_list = i_dict["watched"] 
+        for movies_dict in movies_list:
+            if movies_dict not in user:
+                friends_list.append(movies_dict)
+                if key,value in friends_list["title"]
+    return friends_list
+"""
+
+
+#wave 4
+#function 1
+
+# def get_available_recs(user_data):
+#     """
+#     takes 1 parameter user_data
+#     user_data field subcscriptions
+#     value of subscriptions is list of string
+#     represents the names of ea streaming service user has access
+#     ea friend in friends has watched list
+#     ea movie in watched list has host
+#     host is string that says what streaming service is hosted on
+#     determine list of recs nmovies
+#     movie should be added to list only if 
+#     user has not watched
+#     atleast one user friend has watched
+#     host of movie is service thats in users subscription 
+#     return list of rec movies
+#     """
+#      amandas_data = {
+#         "subscriptions": ["hulu", "disney+"],
+#         "watched": [],
+#         "friends": [
+#             {
+#                 "watched": [HORROR_1b]
+#             },
+#             {
+#                 "watched": [FANTASY_3b]
+#             }
+#         ]
+#     }
+#     #subscription ['netflix','hulu','crunchyroll']
+#     friends = [friend:[movie : "host"(watched list)]]
+#     list_of_rec_movies = []
+
+#     users_subscriptions = user_data[0]
+#     #[friends][0][watched]
+#     movies add to list if 
+#     user has not watched
+#     one friend has watched
+#     host is a service users owns
+#     return list_of_rec_movies
+    
+
+
 # -----------------------------------------
 # ------------- WAVE 4 --------------------
 # -----------------------------------------
