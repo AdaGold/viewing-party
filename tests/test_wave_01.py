fc6d416481378320323e5419797e4cbf060cfa34
shiyinxi <shiyinxi@gmail.com>
2024-09-23 16:11:35 -0700

    wave_01 tests completed

--- tests/test_wave_01.py
+++ tests/test_wave_01.py
@@ -158,6 +158,15 @@ def test_moves_movie_from_watchlist_to_empty_watched():
     # Assert
     assert len(updated_data["watchlist"]) == 0
     assert len(updated_data["watched"]) == 1
+
+    assert updated_data == {
+        "watchlist": [],
+        "watched": [{
+            "title": MOVIE_TITLE_1,
+            "genre": GENRE_1,
+            "rating": RATING_1
+        }]
+    }
     
     # raise Exception("Test needs to be completed.")
     # *******************************************************************************************
@@ -183,6 +192,12 @@ def test_moves_movie_from_watchlist_to_watched():
     assert len(updated_data["watchlist"]) == 1
     assert len(updated_data["watched"]) == 2
     
+    assert updated_data == {
+        "watchlist": [
+            FANTASY_1
+        ],
+        "watched": [FANTASY_2, HORROR_1]
+    }
     # raise Exception("Test needs to be completed.")
     # *******************************************************************************************
     # ****** Add assertions here to test that the correct movie was added to "watched" **********
@@ -205,3 +220,6 @@ def test_does_nothing_if_movie_not_in_watchlist():
     assert len(updated_data["watched"]) == 1
     assert movie_to_watch not in updated_data["watchlist"]
     assert movie_to_watch not in updated_data["watched"]
+
+    
+
