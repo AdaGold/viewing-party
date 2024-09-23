7ba7fd245b973c701cf7102f0880df8fceb4a726
DC <denise.p.cheng@gmail.com>
2024-09-23 14:48:30 -0400

    first wave 1 completed with tests passing (still need to work on exceptions)

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,7 +1,46 @@
 # ------------- WAVE 1 --------------------
 
 def create_movie(title, genre, rating):
-    pass
+    if not title or not genre or not rating:
+        return None
+    
+    new_movie_info = {"title": title, "genre": genre, "rating": rating}
+    return new_movie_info
+
+def add_to_watched(user_data, movie):
+    
+    # user_data = {key="name_of_movie_being_watched": value = a list of dictionaries representing the movies the user has watched}
+    # empty list for if the user has no movies in their watched list
+    user_data["watched"].append(movie)
+    return user_data
+
+def add_to_watchlist(user_data, movie):
+    user_data["watchlist"].append(movie)
+    return user_data
+
+def watch_movie(user_data, title):
+    for movie in user_data["watchlist"]:
+        if movie["title"] == title:
+            user_data["watchlist"].remove(movie)
+            user_data["watched"].append(movie)
+    return user_data
+
+    '''### Wave 1
+4. Create a function named `watch_movie`. This function should...
+
+- take two parameters: `user_data`, `title`
+  - the value of `user_data` will be a dictionary with a `"watchlist"` and a `"watched"`
+    - This represents that the user has a watchlist and a list of watched movies
+  - the value of `title` will be a string
+    - This represents the title of the movie the user has watched
+- If the title is in a movie in the user's watchlist:
+  - remove that movie from the watchlist
+  - add that movie to watched
+  - return the `user_data`
+- If the title is not a movie in the user's watchlist:
+  - return the `user_data`
+
+Note: For Waves 2, 3, 4, and 5, your implementation of each of the functions should not modify `user_data`.'''
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
